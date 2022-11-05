7e56f040076885f843546caebeab23ba33724121
Filipe Paulino <filipepaulinodeveloper@hotmail.com>
2022-11-05 14:06:40 -0300

    migration dia_aula criada

---
+++ database/migrations/2022_11_05_155311_dia_aula_table_create.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dia_aula', function(Blueprint $table){
+            $table->foreignId('turma_id')->constrained('turmas');
+            $table->time('di');
+            $table->string('horario_aula',14);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
